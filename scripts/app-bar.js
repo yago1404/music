dc6a6c528535dde2f8a7d4b72ed14457426e0577
Gustavo Costa <gcco@ic.ufal.br>
2023-04-14 16:56:02 -0300

    Links and Galeria Page

--- scripts/app-bar.js
+++ scripts/app-bar.js
@@ -1,14 +1,22 @@
-function initAppBar() {
-    document.getElementById('hello-link').style.padding = "4px 10px";
-    document.getElementById('hello-link').style.borderBottom = "3px solid white";
+const initAppBar = function(itemId) {
+    item =  document.getElementById(itemId);
+    item.style.padding = "4px 10px";
+    item.style.borderBottom = "3px solid white";
 }
 
-const clickNavItem = function(itemId) {
-    let elements = document.getElementsByClassName('nav-item');
-    for (let i = 0; i < elements.length; i++) {
-        document.getElementById(elements.item(i).id).style.padding = "0";
-        document.getElementById(elements.item(i).id).style.borderBottom = "0px";
-    }
-    document.getElementById(itemId).style.padding = "4px 10px";
-    document.getElementById(itemId).style.borderBottom = "3px solid white";
+ const clickNavItem = function(itemId) {
+     switch (itemId) {
+        case 'hello-link':
+            window.location.href = 'index.html';
+            break;
+
+         case 'galeria-link':
+             window.location.href = 'galeria.html';
+             break;
+         case 'email':
+             break;
+
+         default:
+             break;
+     }
 }
---
+++ scripts/social-media.js
@@ -0,0 +1,13 @@
+const clickSocialMedia = function(itemId){
+    
+    switch (itemId) {
+        case 'insta':
+            window.location.href = 'https://www.instagram.com/mus.ic.ufal/?igshid=NTdlMDg3MTY%3D';
+            break;
+        case 'email':
+            break;
+    
+        default:
+            break;
+    }
+}
